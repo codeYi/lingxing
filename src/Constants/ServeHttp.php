9ad2c2f7b683d862b726d75cbef37183cc9d5799
yolo_me <yolo_me@163.com>
2023-12-02 09:43:02 +0800

    'api常量新增产品模块'

--- src/Constants/ServeHttp.php
+++ src/Constants/ServeHttp.php
@@ -60,4 +60,90 @@ interface ServeHttp
     //查询售后订单列表
     const QUERY_AFTER_ORDER = '/erp/sc/routing/amzod/order/afterSaleList';
 
+    #==================商品信息===================
+
+    //创建UPC编码
+    const CREATE_UPC = '/listing/publish/api/upc/addCommodityCode';
+
+    //获取upc编码列表
+    const QUERY_UPC_LIST = '/listing/publish/api/upc/upcList';
+
+    //查询本地产品列表
+    const QUERY_PRODUCT_LIST = '/erp/sc/routing/data/local_inventory/productList';
+
+    //查询本地产品详情
+    const QUERY_PRODUCT_INFO = '/erp/sc/routing/data/local_inventory/productInfo';
+
+    //批量查询产品详情
+    const QUERY_PRODUCT_INFO_BATCH = '/erp/sc/routing/data/local_inventory/batchGetProductInfo';
+
+    //添加/编辑本地产品
+    const UPDATE_PRODUCT = '/erp/sc/routing/storage/product/set';
+
+    //查询产品属性列表
+    const QUERY_PRODUCT_ATTRIBUTE = '/erp/sc/routing/storage/attribute/attributeList';
+
+    //添加 / 编辑产品属性
+    const UPDATE_PRODUCT_ATTRIBUTE = '/erp/sc/routing/storage/attribute/set';
+
+    //查询多属性产品列表
+    const QUERY_MULTI_ATTRIBUTE_PRODUCT = '/erp/sc/routing/storage/spu/spuList';
+
+    //查询多属性产品详情
+    const QUERY_MULTI_ATTRIBUTE_PRODUCT_DETAIL = '/erp/sc/routing/storage/spu/info';
+
+    //添加/编辑多属性产品
+    const UPDATE_MULTI_ATTRIBUTE_PRODUCT_DETAIL = '/erp/sc/routing/storage/spu/set';
+
+    //查询捆绑产品关系列表
+    const QUERY_BUNDLED_PRODUCT = '/erp/sc/routing/data/local_inventory/bundledProductList';
+
+    //添加 / 编辑捆绑产品
+    const UPDATE_BUNDLED_PRODUCT = '/erp/sc/routing/storage/product/setBundled';
+
+    //查询产品辅料列表
+    const QUERY_PRODUCT_AUX = '/erp/sc/routing/data/local_inventory/productAuxList';
+
+    //添加、编辑辅料
+    const UPDATE_PRODUCT_AUX = '/erp/sc/routing/storage/product/setAux';
+
+    //查询产品品牌列表
+    const QUERY_PRODUCT_BRAND = '/erp/sc/data/local_inventory/brand';
+
+    //添加/编辑产品品牌
+    const UPDATE_PRODUCT_BRAND = '/erp/sc/storage/brand/set';
+
+    //查询产品分类列表
+    const QUERY_PRODUCT_CATEGORY = '/erp/sc/routing/data/local_inventory/category';
+
+    //添加/编辑产品分类
+    const UPDATE_PRODUCT_CATEGORY = '/erp/sc/routing/storage/category/set';
+
+    //上传本地产品图片
+    const UPLOAD_PRODUCT_URL = '/erp/sc/routing/storage/product/uploadPictures';
+
+    //查询产品标签
+    const QUERY_PRODUCT_LABEL = '/label/operation/v1/label/product/list';
+
+
+    //创建产品标签
+    const CREATE_PRODUCT_LABEL = '/label/operation/v1/label/product/create';
+
+
+    //标记产品标签
+    const MARK_PRODUCT_LABEL = '/label/operation/v1/label/product/mark';
+
+    //删除产品标签
+    const DELETE_PRODUCT_LABEL = '/label/operation/v1/label/product/unmarkLabel';
+
+
+
+
+
+
+
+
+
+
+
 }
